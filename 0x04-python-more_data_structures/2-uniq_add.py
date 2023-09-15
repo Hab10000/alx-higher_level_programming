f62010372914e464bf7b5f260e55b3a4967de4aa
Hab1000 <Alazarembakom198@gamil.com>
2023-09-15 04:29:05 -0700

    unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
 
+    for i in uniq_list:
+        num += i
 
-def common_elements(set_1, set_2):
-    """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+    return (num)

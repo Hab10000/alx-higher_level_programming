2b60d0c4f6e184050cbd903d32f3564e8658cbb9
Hab1000 <Alazarembakom198@gamil.com>
2023-09-22 00:28:29 -0700

    lists

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
 
-def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
     """
-    A function that prints a matrix of integers.
+    A function that adds 2 tuples
     """
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != (len(matrix[i]) - 1):
-                print(" ", end="")
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
-        print("")
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

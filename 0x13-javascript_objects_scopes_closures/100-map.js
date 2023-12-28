b1713bcb1afda501f3932695bfd8a20aa2b867a6
Hab10000 <Alazarembakom198@gmail.com>
2023-12-28 04:50:02 -0800

    .

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
-#!/usr/bin/node
-const Mylist = require('./100-data.js').list;
-const NewList = Mylist.map((x, y) => x * y);
-console.log(Mylist);
-#!/usr/bin/node
 const Mylist = require('./100-data.js').list;
 const NewList = Mylist.map((x, y) => x * y);
 console.log(Mylist);

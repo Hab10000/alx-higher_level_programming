8cb33cc0569c9317ba63e83deb4763605472d01f
Hab10000 <Alazarembakom198@gmail.com>
2023-12-28 04:35:06 -0800

    .

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    const NewList = [];
+    for (let i = 0; i < list.length; i++) {
+      NewList.unshift(list[i]);
+    }
+    return NewList;
+  };
+  
